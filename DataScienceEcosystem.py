c596ba7ce41869b1119cb67ce8d6055c4fc17346
DrRobertB <98327498+DrRobertB@users.noreply.github.com>
2023-07-30 14:13:54 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Exercise 1: Create a Jupyter Notebook
+# 
+# ### <span style ="color:Green">  *The Jupyter Notebook saved as DataScienceEcosystem* </span>
+
+# ## Exercise 2: Create marckdown cell with title of notebook
+# 
+# 
+# 
+# # <span style="color:Green"> *Data Science Tools and Ecosytem*</span>
+
+# ## Exercise 3: Create markdown cell for an introduction 
+# 
+# ### <span style="color:green"> *In this notebook, Data Science Tools and Ecosystem are summarized.* </span>
+
+# ## Exercise 4: Create a markdown cell to list data science languages
+# 
+# 
+# <span style="color:green">
+#       
+#     
+# ### ***Some of the popular languages that Data Scientist use are:***
+#     
+# 1. ***Python***   
+# 1. ***R***    
+# 1. ***SQL***    
+# 1. ***Others Languages***
+#     - Scala
+#     - Julia
+#     - C++
+#    
+# 
+#  </span>
+# 
+
+# ## Exercise 5: Create a markdown cell to list data scince libraries 
+# 
+# <span style="color:green">
+#     
+# ### ***Some of commonly used libraries used by Data Scientist include:***
+# 
+# 1. ***Scientific computing libraries***
+#     1. *Panda*
+#     1. *NumPy*
+# 1. ***Visualization libraries***
+#     1. *Matplotlib*
+#     1. *Seaborn*
+# 1. ***Deep learning libraries in python***
+#     1. *Tensorflow*
+#     1. *Pytoech*
+#     
+# </span>
+
+# ## Exercise 6: Create a markdown cell with a table of Data Science tools.
+# 
+# 
+# 
+# |<span style="color:green">***Data Science Tools***</span>|
+# |-----|
+# |<span style="color:green">*Jupyter*</span>|
+# |<span style="color:green">*Apache Zeppeline*</span>|
+# |<span style="color:green">*RStudio*</span>
+# 
+# 
+#     
+#     
+#     
+#     
+#     
+# 
+
+# ## Exercise 7: Create a markdown cell introducing arithamatic expression examples
+# 
+# 
+# 
+# <span style="color:green">
+# 
+# ###    *Below are a few example of evaluting arithamatic expression in Python.*</span>
+
+# ## Exercise 8: Create a code to multiply and add number
+
+# In[10]:
+
+
+#(3*4)+5
+Answer<-(3*4)+5
+print("(3*4)+5=",Answer)
+
+
+# ## Exercise 9: Create a code cell to convert minute to hours
+
+# In[14]:
+
+
+#This will convert 200 minutes to hours by dividing 60.
+Hour<-200/60
+print("200 minutes is equal to", Hour,"Hours")
+
+
+# ## Exercise 10: Insert a markdown cell to list  Objectives 
+# 
+# <span style="color:green">
+# 
+# *Objectives*
+# - Python
+# - R
+# - SQL
+# - Scala
+# - Julia
+#     
+# </span>
+
+# ## Exercise 11: Create a markdown cell to indiacate the Author's name
+# 
+# 
+# ##  <span style="color:green"> Sarthaj Aram Kuni </span>
+# 
+
+# In[ ]:
+
+
+
+
